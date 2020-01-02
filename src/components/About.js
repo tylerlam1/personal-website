54615880abdd265f62b90f0a0f66d9cb709a9846
unknown <tylerl264@gmail.com>
2020-01-02 15:25:42 -0700

    fix view on mobile device

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ function About() {
             <div className='text'>
                 <h1>Tyler Lam</h1>
                 <p>
-                    Hi! I'm Tyler. I'm currently a student at the University of Calgary. I'm current majoring in Software Engineering with a specialization in Biomedical Engineering.
+                    Hi! I'm Tyler. I'm currently a student at the University of Calgary. I'm majoring in Software Engineering with a specialization in Biomedical Engineering.
                     <br />
                     <br />
                     I'm currently a student researcher in the <a href="https://www.aaronphillipslab.com/">Phillips Lab</a>, and previously have had the pleasure of working as an Information Systems intern at <a href="http://www.paramountres.com/">Paramount Resources</a>. At school, I'm also part of the Let's Talk Science team, focused on science education and advocacy among youth. Learn more about them <a href="https://outreach.letstalkscience.ca/ucalgary.html">here.</a>
