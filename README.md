98db6fc8455b2b111b184b3de8376cd49b6a696c
unknown <tylerl264@gmail.com>
2020-03-26 01:09:46 -0600

    lazy load picture and reduce quality

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app) and hosted by Firebase.
-
-You can visit it [here](https://just-clover-212517.firebaseapp.com/). Alternatively, you can download and run 
-
-```console
-npm start
-```
-
-Which runs the application in development mode. Open http://localhost:3000 to view it in the browser.
+You can visit my website [here](https://tylerlam.me/). Alternatively, you can download and run `npm i` to install all dependencies and `npm start` to run in development mode.
--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,38 @@
-import React from 'react'
-import mainBanner from '../assets/PersonalPicture.jpg'
-import Buttons from './Buttons';
-import '../styles/About.css'
+import React from "react";
+import mainBanner from "../assets/PersonalPicture.jpg";
+import Buttons from "./Buttons";
+import LazyLoad from "react-lazyload";
+import "../styles/About.css";
 
 const About = () => {
-    return (
-        <div className = 'about'>
-            <div className = 'banner'>
-                <img src={mainBanner}/>
-            </div>
-            <div className='text'>
-                <h1>Tyler Lam</h1>
-                <p>
-                    Hi! I'm Tyler. I'm currently a student at the University of Calgary. I'm majoring in Software Engineering with a specialization in Biomedical Engineering.
-                    <br />
-                    <br />
-                    I'm currently a student researcher in the <a href="https://www.aaronphillipslab.com/">Phillips Lab</a>, and previously have had the pleasure of working as an Information Systems intern at <a href="http://www.paramountres.com/">Paramount Resources</a>. At school, I'm also part of the Let's Talk Science team, focused on science education and advocacy among youth. Learn more about them <a href="https://outreach.letstalkscience.ca/ucalgary.html">here.</a>
-                </p>
-            </div>
-            <Buttons />
-        </div>
-    )
-}
+  return (
+    <div className="about">
+      <div className="banner">
+        <LazyLoad once>
+          <img src={mainBanner} />
+        </LazyLoad>
+      </div>
+      <div className="text">
+        <h1>Tyler Lam</h1>
+        <p>
+          Hi! I'm Tyler. I'm currently a student at the University of Calgary.
+          I'm majoring in Software Engineering with a specialization in
+          Biomedical Engineering.
+          <br />
+          <br />
+          I'm currently a student researcher in the{" "}
+          <a href="https://www.aaronphillipslab.com/">Phillips Lab</a>, and
+          previously have had the pleasure of working as an Information Systems
+          intern at{" "}
+          <a href="http://www.paramountres.com/">Paramount Resources</a>. At
+          school, I'm also part of the Let's Talk Science team, focused on
+          science education and advocacy among youth. Learn more about them{" "}
+          <a href="https://outreach.letstalkscience.ca/ucalgary.html">here.</a>
+        </p>
+      </div>
+      <Buttons />
+    </div>
+  );
+};
 
 export default About;
